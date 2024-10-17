c6670e97338bc6550505a7d6e349ec8d02e636eb
NDshinde72 <163769697+NDshinde72@users.noreply.github.com>
2024-10-17 22:10:32 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.contact-btn').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Contact functionality is not implemented yet.');
+    });
+});
